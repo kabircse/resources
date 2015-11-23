2804d5dd78f9038bf5628a36d10c958bcf50e365
Kabir Hossain <kabir.pust@gmail.com>
2015-11-23 10:56:29 +0600

    Update and rename composer.php to composer.md

--- composer.php
+++
@@ -1,8 +0,0 @@
-*Composer Installation:*
-Composer:
-1. Download composer-setup.exe to pc and install it.
-2. Using cmd go to project location
-3. Run php -r "readfile('https://getcomposer.org/installer');" | php
-3.php composer.phar self-update
- php composer.phar install 
-
